ab1466ca4b698319ba34db4868ccc93b57bbb035
Fontaine <-->
2014-07-16 17:54:48 +0200

    config simplification

--- config-tobefilled.php
+++ config-tobefilled.php
@@ -13,12 +13,10 @@ $CONFIG=array();
 
 	$CONFIG['siteTitle'] = 'filmo';
 	$CONFIG['homepage_movies_by_category'] = 10;
-	$CONFIG['movies_by_pages'] = 5; // number of movies by categories.
-	$CONFIG['shortcut_by_page'] = 5; // nombre de numéro de page en bas de l'index !! impaire
 
 /*----------- MYSQL CONFIGURATION -------------*/
 
-	$CONFIG['options']  = array(PDO::MYSQL_ATTR_INIT_COMMAND    => "SET NAMES utf8"); // ! don't change exept
-	$CONFIG['dsn'] = 'mysql:dbname='.$CONFIG['dbName'].';host='.$CONFIG['serverHost']; // ! don't change exept
+	$CONFIG['options']  = array(PDO::MYSQL_ATTR_INIT_COMMAND    => "SET NAMES utf8"); // ! don't change exept you know what you'r doing...
+	$CONFIG['dsn'] = 'mysql:dbname='.$CONFIG['dbName'].';host='.$CONFIG['serverHost']; // ! don't change exept you know what you'r doing...
 
 
